5b81e9842cf414c3d8935e2f7462078de240d80b
mystique <13oct08@quantumfoam.uni.me>
2016-02-15 12:27:42 +0530

    Multiples updates to the CSS part of the form & how the image is presented
    ToDo
    - [] Google for the actor , director names and show images?

--- appSrc/server.js
+++ appSrc/server.js
@@ -20,15 +20,23 @@ app.get('/', function(req, res){
   // The form's action is '/' and its method is 'POST',
   // so the `app.post('/', ...` route will receive the
   // result of our form
-  var html = '<form action="/" method="post">' +
-               'Media Title:' +
-               '<input type="text" name="mediaTitle" placeholder="..." />' +
-               '<br>' +
-               'Year:' +
-               '<input type="text" name="mediaYear" placeholder="..." />' +
-               '<button type="submit">Submit</button>' +
-             '</form>' +
-             '<br />Node.js container running on' + hostname  + '<br />';
+  var html ="<html><head><style>" +
+            "#rcorners {border-radius: 25px;border: 2px solid #ccccff;padding: 20px;}" +
+            "#rbutton {background-color: #008CBA;border: 1px;border-radius: 8px;color: white;padding: 12px 12px;text-align: center;text-decoration: none;display: inline-block;font-size: 16px;}" + 
+            "#rtextbox {width: 300px; height: 40px; padding: 1px;font-size: 20pt}" +			
+			"</style></head><body>";
+  html = html + '<br>' +
+                '<br>' +
+			  	'<form id="rcorners" action="/" method="post">' +
+                '<b style="font-size: 18pt">Media Title:</b>' +
+                '<input id="rtextbox" type="text" name="mediaTitle" placeholder="..." />' +
+                '&nbsp;&nbsp;' +
+				'<b style="font-size: 18pt">Year:</b>' +
+                '<input id="rtextbox" type="text" name="mediaYear" maxlength="4" placeholder="..." />' +
+			    '&nbsp;&nbsp;' +
+                '<button id="rbutton" type="submit">Search for media</button>' +
+                '</form>' +
+                '<br /><br />Node server is hosted on <i>' + hostname  + '</i><br /></body></head></html>';
                
   res.send(html);
 });
@@ -39,7 +47,6 @@ app.post('/',function(req,res){
   var mYear = req.body.mediaYear;  
   var request = require('request');
   //http://www.omdbapi.com/?t=titanic&y=1997&plot=short&r=json
-  
   var options = {
 				url: 'http://www.omdbapi.com/?&plot=short&r=json&t='+mTitle+'&y='+mYear,
 				headers:{
@@ -51,24 +58,24 @@ function callback(error, response, body) {
 	if (!error && response.statusCode == 200) {
 		var mArray = JSON.parse(body);
 		var prepContent="";
+		var mContent="<html><head><style>#rcorners {border-radius: 25px;border: 2px solid #ccccff;padding: 20px;} tr:nth-child(even) {background-color: #efeff5;}</style></head><body>";
+		mContent = mContent + "<br /><br /><h3>Your media of choice is <b><i>"+mTitle+"</b></i>, Year of release is '"+mYear+"', Thats a good one!</h3>"
 		for (var key in mArray) {
 			if (key != "Poster"){
-				prepContent = prepContent +"<tr><td>"+key+":</td><td>"+mArray[key]+"</td></tr>";
+				prepContent = prepContent + "<tr><td>"+key+":</td><td>"+mArray[key]+"</td></tr>";
 			}
 			else {
-				prepContent = prepContent +"<tr><td>"+key+":</td><td><img src='"+mArray[key]+"' alt='Movie Poster'></td></tr>";
+				prepContent = prepContent + "<tr><td>"+key+":</td><td><a href='"+mArray[key]+"'><img src='"+mArray[key]+"'alt='Movie Poster'></a></td></tr>";
 			}
-			// console.log(body)
-			var mContent = "<table>"+ prepContent +"</table>";
-			var html = mContent + "<br /><br />The Media of choice is <b><i>"+mTitle+"</b></i>, Year of release is "+mYear+", Thats a good one!";
-			res.send(html);
 		}
+        // console.log(body)
+        var mContent = mContent + "<table id='rcorners'>"+ prepContent +"</table></body></head></html>";
+        res.send(mContent);
 	}
 	else {
-	  res.send(error);
+        res.send(error);
 	}
 }
-  
 request(options, callback);
 });
 
